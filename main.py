b5b33f161469697a82e6b7d760964c005be3a4b6
smussenden <smussenden@gmail.com>
2021-02-11 11:22:23 -0500

    adding heroku necessaries

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# To get versions in requirements.txt pip freeze > requirements.txt
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,8 +10,17 @@ import time
 
 def scrape_top_news():
 
+    # Add in chrome options for heroku
+    # Including headless browser
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
     browser = webdriver.Chrome(
-        executable_path="/Users/akjasim/chromedriver/chromedriver")
+        executable_path="/Users/macadmin/Desktop/git_repos/chromedriver/chromedriver")
 
     wait = WebDriverWait(browser, 10)
     browser.get('https://news.ycombinator.com/')
